e34868ab07979cd69513b45d07d8a7ec810f9e73
Jason Arnold <jason.arnold.00@gmail.com>
2017-06-09 23:07:43 -0400

    add new todo

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import '../css/app.css';
+import '../assets/css/app.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      input : '',
       toDos : ['eat lunch', 'walk dog']
     };
 
     this.renderToDos = this.renderToDos.bind(this);
     this.addToDo = this.addToDo.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({input: e.target.value});
   }
 
   renderToDos() {
@@ -21,8 +27,11 @@ class App extends Component {
   }
 
   addToDo(e) {
-    e.preventDefault()
-    console.log('submit clicked')
+    e.preventDefault();
+    let noteContent = this.state.input;
+    let toDos = this.state.toDos;
+    toDos.push(noteContent);
+    this.setState({input: '', toDos});
   }
 
   render () {
@@ -31,7 +40,7 @@ class App extends Component {
         <h1>Personal Kanban</h1>
         <form>
           <label htmlFor="noteContent">Note Content: </label>
-          <input type="text" name="noteContent" id="noteContent"></input>
+          <input type="text" name="noteContent" id="noteContent" value={this.state.input} onChange={this.onInputChange}></input>
           <input type="radio" name="color" value="yellow"></input>Yellow
           <input type="radio" name="color" value="green"></input>Green
           <input type="radio" name="color" value="blue"></input>Blue
