af7462c58ac813c8f9943bfd849243d3ead49da1
Jason Arnold <jason.arnold.00@gmail.com>
2017-06-09 22:52:59 -0400

    rebuild

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,48 @@
-import React, { Component } from 'react';
-import '../assets/css/app.css';
-import Header from './Header'
-import AddForm from './AddForm'
-import Container from './Container'
+import React, { Component } from 'react'
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      toDoNotes: [],
-      currentProjectNote: [],
-      doneNotes: [
-        {key: 1, color: 'red', content: 'eat lunch'}
-      ]
+      toDos : ['eat lunch', 'walk dog']
     };
 
-    this.addNote = this.addNote.bind(this);
-    this.removeNote = this.removeNote.bind(this);
+    this.renderToDos = this.renderToDos.bind(this);
+    this.addToDo = this.addToDo.bind(this);
   }
 
-  render() {
+  renderToDos() {
+    let todos = this.state.toDos;
+    let resultArray = [];
+    todos.map((todo, i) => resultArray.push(<li key={i}>{todo}</li>));
+    return resultArray;
+  }
+
+  addToDo(e) {
+    e.preventDefault()
+    console.log('submit clicked')
+  }
+
+  render () {
     return (
-      <div className="App">
-        <Header />
-        <AddForm
-          state={this.state}
-          addNote={this.addNote}
-        />
-        <Container
-          notesInContainer={this.state.toDoNotes.length}
-          classes={"container"}
-          notes={this.state.toDoNotes}
-          title={"To Do"}
-          removeNote={this.removeNote}
-        />
-        <Container
-          notesInContainer={this.state.currentProjectNote.length}
-          classes={"container currentProject"}
-          notes={this.state.currentProjectNote}
-          title={"Current Project"}
-          removeNote={noteNumber => this.setState({ currentProjectNote: this.state.currentProjectNote.filter((note) => {
-            return note.key !== noteNumber
-          })})}
-        />
-        <Container
-        notesInContainer={this.state.doneNotes.length}
-        classes={"container"}
-        notes={this.state.doneNotes}
-        title={"Done"}
-        removeNote={noteNumber => this.setState({ doneNotes: this.state.doneNotes.filter((note) => {
-          return note.key !== noteNumber
-        })})}
-        />
+      <div className="container">
+        <h1>Personal Kanban</h1>
+        <form>
+          <label htmlFor="noteContent">Note Content: </label>
+          <input type="text" name="noteContent" id="noteContent"></input>
+          <input type="radio" name="color" value="yellow"></input>Yellow
+          <input type="radio" name="color" value="green"></input>Green
+          <input type="radio" name="color" value="blue"></input>Blue
+          <input type="radio" name="color" value="orange"></input>Orange
+          <br />
+          <input type="submit" value="Submit" onClick={this.addToDo}></input>
+        </form>
+        <div className="toDo box">
+          {this.renderToDos()}
+        </div>
       </div>
-    );
-  }
-
-  addNote(newNote) {
-    this.setState({ toDoNotes: [...this.state.toDoNotes, newNote]})
-  }
-
-  removeNote(noteNumber) {
-    this.setState({ toDoNotes: this.state.toDoNotes.filter((note) => {
-      return note.key !== noteNumber
-    })})
-  }
-
-  makeNoteCurrent(noteNumber) {
-
+    )
   }
 }
 
--- src/components/DeleteButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DeleteButton = ({removeNote, noteNumber}) => {
-  return (
-      <button onClick={() => removeNote(noteNumber)}>Delete</button>
-  )
-};
-
-export default DeleteButton;
